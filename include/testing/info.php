bd3d862ce18a44092b08b8c410ae7c8cb7bac9eb
RomanZSeldinov <74790793+RomanZSeldinov@users.noreply.github.com>
2022-12-29 11:01:40 +0000

    Some micro changes.

--- include/testing/info.php
+++ include/testing/info.php
@@ -1,6 +1,5 @@
 <?php
 session_start();
-echo('API Nickolas');
 $_SESSION['message'] = 'ПАРУСНИК версия от 27/12/22(b0.1)';
 header('Location: ../../login.php');
 ?>
--- include/testing/return.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_start();
-$_SESSION['message'] = 'entire script error';
-header('Location: ../../login.php');
-?>
